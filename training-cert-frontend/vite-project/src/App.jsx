2b367f381872c80ecf7d52d21426c6d156d3b15d
Bill Shewan <bill.shewan@utiligent.ca>
2025-05-20 09:31:09 +1000

    fixing path for uploader

--- training-cert-frontend/vite-project/src/App.jsx
+++ training-cert-frontend/vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import ExcelTemplateUploader from './components/ExcelTemplateUploader'
+import ExcelTemplateUploader from 'src/components/ExcelTemplateUploader'
 
 
 function App() {
